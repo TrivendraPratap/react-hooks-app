bbe12e11ab47aa579f8ca23ee036be94eb06ad4c
TrivendraPratap <trivendra055@gmail.com>
2024-12-09 21:55:10 +0530

    Simple commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+// import CounterApp from "./components/CounterApp";
+import ImageApp from "./components/ImageApp";
+import CounterApp from "./components/CounterApp";
+
+const App = () => {
+  return (
+    <div>
+      <ImageApp/>
+      {}
+      {/* <CounterApp/> */}
+      
+    </div>
+  );
+};
+
+export default App;
